d91540596b70a098245aaaa1c6552bf14ce05852
Taylor DH Smith <contact@tdhsmith.com>
2013-02-01 16:40:12 -0700

    Added gitignore (+ ignored local batch files)
    Built some local batch files for easier copying and now I'm gitignoring
    them.

---
+++ copy_from_texlocal.bat
@@ -0,0 +1,12 @@
+XCOPY C:\texlive\texmf-local\tex\latex\local\tifinagh\tifinagh.sty C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+XCOPY C:\texlive\texmf-local\tex\latex\local\tifinagh\ETIFenc.def C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+XCOPY C:\texlive\texmf-local\tex\latex\local\tifinagh\ETIFIrcamUnicode.fd C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+XCOPY C:\texlive\texmf-local\tex\latex\local\tifinagh\ETIF_original.enc C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+
+XCOPY C:\texlive\texmf-local\fonts\vf\local\tifinagh\Tifinaghe-IrcamUnicode--ETIF.vf C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+
+XCOPY C:\texlive\texmf-local\fonts\tfm\local\tifinagh\Tifinaghe-IrcamUnicode--ETIF.tfm C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+XCOPY C:\texlive\texmf-local\fonts\tfm\local\tifinagh\Tifinaghe-IrcamUnicode--ETIF--base.tfm C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+
+XCOPY C:\texlive\texmf-local\fonts\map\pdftex\local\tifinagh\ETIF.map C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
+XCOPY C:\texlive\texmf-local\fonts\enc\local\tifinagh\ETIF_gen.enc C:\Users\tdhs\Documents\GitHub\latex-tifinagh\ /Y
---
+++ copy_to_texlocal.bat
@@ -0,0 +1,12 @@
+XCOPY tifinagh.sty C:\texlive\texmf-local\tex\latex\local\tifinagh\ /Y
+XCOPY ETIFenc.def C:\texlive\texmf-local\tex\latex\local\tifinagh\ /Y
+XCOPY ETIFIrcamUnicode.fd C:\texlive\texmf-local\tex\latex\local\tifinagh\ /Y
+XCOPY ETIF_original.enc C:\texlive\texmf-local\tex\latex\local\tifinagh\ /Y
+
+XCOPY Tifinaghe-IrcamUnicode--ETIF.vf C:\texlive\texmf-local\fonts\vf\local\tifinagh\ /Y
+
+XCOPY Tifinaghe-IrcamUnicode--ETIF.tfm C:\texlive\texmf-local\fonts\tfm\local\tifinagh\ /Y
+XCOPY Tifinaghe-IrcamUnicode--ETIF--base.tfm C:\texlive\texmf-local\fonts\tfm\local\tifinagh\ /Y
+
+XCOPY ETIF.map C:\texlive\texmf-local\fonts\map\pdftex\local\tifinagh\ /Y
+XCOPY ETIF_gen.enc C:\texlive\texmf-local\fonts\enc\local\tifinagh\ /Y
